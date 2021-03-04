7972fc3961ce7a175bfd6d8725b807a29bb4624e
BEN <benjamin.hill28@gmail.com>
2021-03-04 09:43:20 -0800

    added get_user_model to Snacks model and fixed my issues

--- snacks/models.py
+++ snacks/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from datetime import datetime, timezone
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 class Snacks(models.Model):
     snack_name = models.CharField(max_length=64, help_text="Enter a SNACK!")
     description = models.TextField()
+    snack_amount = models.IntegerField(default=10)
     purchaser = models.ForeignKey(
-        "self",
+        get_user_model(),
         on_delete=models.CASCADE,
     )
     pub_date = models.DateTimeField("date published")
